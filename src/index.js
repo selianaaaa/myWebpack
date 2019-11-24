617a48a2bc19430509012d76cf876e30189115ab
Julia Sanina <cany96@bk.ru>
2019-11-25 01:00:49 +0300

    css and js files changed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './styles/main.scss';
+import App from './js/app'
+  
+const app = new App()
+console.log(app.fullName)
---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+class App{
+    constructor(){
+        this.name = 'App webpack'
+    }
+
+    get fullName(){
+        return `this app name is "${this.name}`
+    }
+}
+
+export default App
